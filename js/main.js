159620025a796fa07dbb901d3d986e5c052e07df
Nathaniel Sena <nate.sena1@gmail.com>
2017-08-29 23:20:28 -0700

    general logic done

--- js/main.js
+++ js/main.js
@@ -14,12 +14,15 @@ equalsBtn.addEventListener('click', function(){
 function isSame(btn){
     console.log('isSame evalArray start: '+ evalArray);
     if(!!readout.innerText){//if not empty
-        if(isNaN(readout.innerText[readout.innerText.length - 1]) === isNaN(btn.innerText)){ 
-            console.log('IS SAME As last character');
+        console.log('last readout not number y/n: ' + isNaN(readout.innerText[readout.innerText.length - 1]) + ' last readout value: '+ (readout.innerText[readout.innerText.length - 1]));
+        console.log('last button not number y/n: ' + isNaN(btn.innerText)+ ' last button value: ' +btn.innerText);
+        //if(isNaN(readout.innerText[readout.innerText.length - 1]) === isNaN(btn.innerText)){ 
+        if(isNaN(readout.innerText[readout.innerText.length - 1]) === isNaN(btn.innerText)){     
+            console.log('is same type as last character');
             return true;
         }
         else{
-            console.log('IS NOT SAME as last character');
+            console.log('Is not same type as last character');
             return false;
         }
     }
@@ -37,13 +40,14 @@ digits.forEach(function(btn){
         console.log('digits evalArray start: '+evalArray);
         if(!!evalArray[0]){//if array is not empty
             console.log('evalArray is not empty');
-            readout.innerText += this.innerText;
             if(isSame(btn)){
+                readout.innerText += this.innerText;
                 console.log('another number was pressed');
                 evalArray[evalArray.length - 1]+=this.innerText;
                 console.log('digits added '+this.innerText+ ' to nonempty evalArray');
             }
             else{
+                readout.innerText += this.innerText;
                 console.log('just switched from operator to number');
                 evalArray.push(this.innerText);
                 console.log('pushed new number to array');
@@ -61,39 +65,18 @@ digits.forEach(function(btn){
     })
 })
 
-// digits.forEach(function(btn){
-//     btn.addEventListener('click', function(){
-//         console.log('digits evalArray start: '+evalArray);
-//        if(isSame(btn)){
-//         readout.innerText += this.innerText;
-//         if(!!evalArray[0]){//if evalArray is empty
-//             evalArray.push(this.innerText);
-//             console.log('digits added '+this.innerText+ ' to empty evalArray');
-//         }
-//         else{
-//             evalArray[evalArray.length - 1]+=this.innerText;
-//         console.log('digits added '+this.innerText+ ' to current indice');
-//         }
-//        }
-//        else{
-//         evalArray.push(this.innerText);
-//         console.log('digits: last character was different, created new index');
-//        }
-//        console.log('digits evalArray end: '+ evalArray);
-//        console.log('--------------------');
-//     })
-// })
-
-
 
 operators.forEach(function(btn){
     btn.addEventListener('click', function(){
+        console.log('operators evalArray start: '+evalArray);
         if(isSame(btn)){
             alert('u cant do that');
         }
         else{
             readout.innerText += this.innerText;
+            console.log('pushing operator into evalArray');
             evalArray.push(this.innerText);
+            console.log('operators evalArray finish: '+evalArray);
         }
         console.log('--------------------');
     })
